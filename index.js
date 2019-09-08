7a316194822cbf1bf4e6f0657424bf1cdede9ba6
Craftist <siteclistru@gmail.com>
2019-09-08 22:12:48 +0300

    New version!

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { VK } = require('vk-io');
+﻿const { VK } = require('vk-io');
 const bot = new VK({
     token: 'b069ed89d72a85c13ffb58deb4072f9504d1b6b94f1ef66004e54903f031f5be78a1030610664f24b220f'
 });
 
 const fs = require('fs');
 
+let users = {}; // keys=user_ids, values=booleans
+
 let database = {}; // keys=peer_ids, values={ key=message, value=attachment }
 
-if (fs.existsSync('db.json')) {
-    database = JSON.parse(fs.readFileSync('db.json'));
+if (fs.existsSync('./db.json')) {
+    console.log(fs.readFileSync('./db.json').toString('utf8'));
+    database = JSON.parse(fs.readFileSync('./db.json').toString('utf8'));
 
     const chatCount = Object.values(database).length;
 
@@ -16,6 +19,8 @@ if (fs.existsSync('db.json')) {
 }
 
 bot.updates.on('new_message', async (ctx) => {
+    if (ctx.isOutbox || ctx.isGroup || ctx.text === null) return;
+
     console.log('[NEW MESSAGE] ' + ctx.text);
     ctx.text = ctx.text.trim();
 
@@ -40,10 +45,7 @@ bot.updates.on('new_message', async (ctx) => {
     }
 
     if (!database[ctx.peerId]) database[ctx.peerId] = {};
-
-    //console.log('database[ctx.peerId]', database[ctx.peerId])
-    //console.log('database[ctx.peerId].zip()', database[ctx.peerId].zip())
-    //console.log('database[ctx.peerId].zip().value', database[ctx.peerId].zip().value)
+    if (users[ctx.senderId] === undefined) users[ctx.senderId] = false;
 
     if (ctx.text.startsWith('/добавить')) {
         let name = ctx.text.substring('/добавить'.length + 1).trim();
@@ -62,10 +64,7 @@ bot.updates.on('new_message', async (ctx) => {
 
         database[ctx.peerId][name] = atts.map(x => x.largePhoto);
         updateDatabase();
-
-        //await ctx.send(`Фотография успешно загружена!`)
         await ctx.send(`Фотографи${atts.length === 1 ? 'я' : 'и'} успешно загружен${atts.length === 1 ? 'а' : 'ы'}!`)
-    //} else if (ctx.text in database[ctx.peerId]) {
     } else if (ctx.text.startsWith('/удалить')) {
         let name = ctx.text.substring('/удалить'.length + 1).trim();
 
@@ -82,13 +81,10 @@ bot.updates.on('new_message', async (ctx) => {
         }
 
         if (deleteByQuery(ctx.peerId, ctx.text)) {
-            await ctx.send(`Фотографи${found.length === 1 ? 'я' : 'и'} успешно загружен${found.length === 1 ? 'а' : 'ы'}!`)
+            await ctx.send(`Фотографи${found.length === 1 ? 'я' : 'и'} успешно удален${found.length === 1 ? 'а' : 'ы'}!`)
         }
 
         updateDatabase();
-
-        //await ctx.send(`Фотография успешно загружена!`)
-    //} else if (ctx.text in database[ctx.peerId]) {
     } else if (ctx.text === '/команды') {
         const cmds = [
             '/добавить <название> -- добавить картинку(картинки) в данную беседу',
@@ -96,14 +92,17 @@ bot.updates.on('new_message', async (ctx) => {
             '/очистить -- удаляет все картинки из указанной беседы. Только для администратора бота.',
             '/список -- выводит список всех картинок в данной беседе.',
             '/команды -- покажет эту справку.',
+            '/гм -- переключит гм. При включенном гм у вас будет возможность писать без команды //.',
             '',
             'Если ввести что-то помимо того, что указано выше, бот попытается воспринять это как название картинки:',
             '<название> -- получить указанную картинку',
             'Названия указываются без слеша (символ "/").',
             '',
             'Пример команд:',
-            '@picture__bot /добавить НАЗВАНИЕ -- добавит прикрепленную(ые) картинку(и) под названием НАЗВАНИЕ.',
-            '@picture__bot НАЗВАНИЕ -- покажет прикрепленную(ые) командой выше картинку(и).',
+            '/добавить НАЗВАНИЕ -- добавит прикрепленную(ые) картинку(и) под названием НАЗВАНИЕ.',
+            '@picture__bot НАЗВАНИЕ -- покажет прикрепленную(ые) командой выше картинку(и). Работает только с гм.',
+            '// НАЗВАНИЕ -- покажет прикрепленную(ые) командой выше картинку(и). Работает только с гм и без него.',
+            'Чтобы избавиться от необходимости писать @picture__bot, как в примере выше, дайте боту право читать все сообщения.',
         ]
 
         await ctx.send('Команды бота:\n' + cmds.map(x => x.length === 0 ? x : '    ⚫ ' + x).join('\n'));
@@ -117,6 +116,44 @@ bot.updates.on('new_message', async (ctx) => {
         updateDatabase();
 
         await ctx.send('Картинки в текущей беседе успешно очищены!');
+    } else if (ctx.text === '/гм') {
+	users[ctx.senderId] = !users[ctx.senderId];
+
+        await ctx.send('Гейммод ' + users[ctx.senderId] ? 'включен!' : 'выключен!');
+    } else if (ctx.text === '/ping') {
+        if (ctx.senderId !== 150013768) {
+            await ctx.send('Пинговать может только администратор бота!');
+            return;
+        }
+
+	let date_1x_users_get = Date.now();
+	await bot.api.users.get({ user_ids: [150013768] });
+	let elapsed_1x_users_get = Date.now() - date_1x_users_get;
+
+	/*let date_10x_users_get = Date.now();
+	for (let i = 0; i < 10; i++) {
+		await bot.api.users.get({ user_ids: [150013768] });
+	}
+	let elapsed_10x_users_get = Date.now() - date_10x_users_get;*/
+
+        await ctx.send(`Ping:\n1x users.get: ${elapsed_1x_users_get}ms`);
+    } else if (ctx.text.startsWith('/жс')) {
+        if (ctx.senderId !== 150013768) {
+            await ctx.send('Доступ к консоли имеет только администратор бота!');
+            return;
+        }
+
+	if (ctx.text.length === 3) {
+            await ctx.send('Введите код!');
+            return;
+	}
+
+	let code = ctx.text.substring(4);
+	let result = await eval(code);
+
+	let type = typeof(result);
+
+	await ctx.send(`${type}: ${result}`);
     } else if (ctx.text === '/список') {
         let list = Object.entries(database[ctx.peerId]).map(triggers => '    ⚫ ' + triggers[0] + ': ' + triggers[1].length + ' элемента(ов)').join('\n');
 
@@ -125,24 +162,13 @@ bot.updates.on('new_message', async (ctx) => {
 
         await ctx.send('Список картинок в данной беседе (это регулярные выражения): ' + list);
     } else {
-        // let found = false;
-
-        // for (triggerRegex in database[ctx.peerId]) {
-        //     let triggerValue = database[ctx.peerId][triggerRegex];
-
-        //     if (RegExp(triggerRegex).test(ctx.text)) {
-        //         await ctx.mySendPhoto(triggerValue, {
-        //             peer_id: ctx.peerId
-        //         });
-        //         found = true;
-        //         break;
-        //     }
-        // }
+        if (ctx.text.startsWith('// ')) {
+            ctx.text = ctx.text.substring(3);
+        }
 
         let found = findByRegex(ctx.peerId, ctx.text);
 
         if (found === null) {
-            //await ctx.send('Такой картинки/команды нет!');
             return;
         }
 
@@ -160,7 +186,7 @@ function findByRegex(peerId, query) {
     for (triggerRegex in database[peerId]) {
         let triggerValue = database[peerId][triggerRegex];
 
-        if (RegExp(triggerRegex).test(query)) {
+        if (RegExp(triggerRegex, 'i').test(query)) {
             return triggerValue;
         }
     }
@@ -170,9 +196,7 @@ function findByRegex(peerId, query) {
 
 function deleteByQuery(peerId, query) {
     for (triggerRegex in database[peerId]) {
-        let triggerValue = database[peerId][triggerRegex];
-
-        if (RegExp(triggerRegex).test(query)) {
+        if (RegExp(triggerRegex, 'i').test(query)) {
             delete database[peerId][triggerRegex];
             return true; // if found return true
         }
